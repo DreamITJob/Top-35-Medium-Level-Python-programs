c387ab8435408082f49205b3923b46fa9c67c1e3
Karan Arora <40701579+DreamITJob@users.noreply.github.com>
2019-07-03 13:11:45 +0530

    Create Program12.py

---
+++ Program12.py
@@ -0,0 +1,7 @@
+#Python program for converting a decimal number into equivalent binary number....
+
+# decimal number
+number = int(input("Enter any decimal number: "))
+
+# print equivalent binary number
+print("Equivalent Binary Number: ", bin(number))
