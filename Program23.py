9820fe6ae29ea1bf05fb9777c26ba04e523ba63d
Karan Arora <40701579+DreamITJob@users.noreply.github.com>
2019-07-11 17:31:41 +0530

    Create Program23.py

---
+++ Program23.py
@@ -0,0 +1,9 @@
+#Python Program for concating of 2 strings....
+
+def Concatenate_String(str1,str2):
+    str3 = str1+str2
+    return str3
+
+str1 = "Karan "
+str2="Arora"
+print(Concatenate_String(str1,str2))
